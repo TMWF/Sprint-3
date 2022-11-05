f5c5b83175846b98d32466949e77106804cf90be
Joe <gpliev777@gmail.com>
2022-11-05 11:20:58 +0300

    Initial commit

--- Sprint 3/ViewController.swift
+++ Sprint 3/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
+    private var counter = 0 {
+        didSet {
+            counterLabel.text = "Значение счетчика: \(counter)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterButton.layer.cornerRadius = 20
+        counterLabel.text = "Значение счетчика: \(counter)"
     }
 
 
+    @IBAction func counterButtonTapped() {
+        counter += 1
+    }
 }
 
